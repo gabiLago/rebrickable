681d96927378779abf2c7924527341a4bc832618
Gabriel Lago Blasco <gabriel@lagoblasco.es>
2019-10-13 19:37:14 +0200

    Initial API call parameteres updated

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ export const BASE_URL = 'https://rebrickable.com/api/v3/lego/';
 export const API_KEY = '59a3c1ca8129bc17e3ce73afcdb06319';
 export const API_ITEMS_LIMIT = 10;
 export const API_INITIAL_PARAMS = {
-  min_year: 2018,
+  min_year: 2010,
   max_year: 2019,
   theme_id: 1,
 };
